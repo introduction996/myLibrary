cdafd6f60a365bcf851b531e38bce4d76b563c33
johnny <ionutsbotnaru@gmail.com>
2023-09-29 21:49:43 +0300

    change read toggle button functionality : now redraws only the current element

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //addBook [X]
 //open form [X]
 //take form answers into an object [X]
-//make book card from those answers []
+//make book card from those answers [X]
+
 //link book card to its respective object in the list []
 //be able to change the read status of a card []
 //be able to delete a card []
 //be able to cancel the form []
 
 const myLibrary = [];
+const libraryShelf = document.querySelector(".books");
 const addBookButton = document.querySelector("#addBook");
 const modal = document.querySelector("#modal");
 addBookButton.addEventListener('click', () => {
@@ -28,17 +30,13 @@ form.addEventListener('submit', (e) => {
   myLibrary.push(objectFromFormData);
   objectFromFormData.dataAttr = String(dataAttribute());
 
-  console.log(myLibrary);
-  console.log(`object keys: ${Object.keys(objectFromFormData)}`);
   form.reset();
   modal.close();
 
-  drawLibrary(objectFromFormData);
+  libraryShelf.appendChild(drawLibrary(objectFromFormData));
 });
 
 
-const libraryShelf = document.querySelector(".books");
-// draw the library
 function drawLibrary(obj) {
   const newDiv = document.createElement('div');
   newDiv.setAttribute('class', 'card');
@@ -50,12 +48,24 @@ function drawLibrary(obj) {
   pages = obj.nrofpages;
   readStatus = obj.readstatus;
   category = obj.category;
-  const cardMarkup = `<h1 id="card-title">${title}</h1>` + 
-  `<h3 id="card-author">${author}</h3>` + 
-  `<h3 id="card-pages">Nr. of pages: ${pages}</h3>` + 
-  `<h4 id="card-read">Reading status: ${readStatus}</h4>` + 
-  `<h4 id="card-category">Category: ${category}</h4>`;
-  newDiv.innerHTML = cardMarkup;
 
-  libraryShelf.appendChild(newDiv);
+  const toggleReadButton = document.createElement('button');
+  toggleReadButton.setAttribute('class', 'read-toggle');
+  toggleReadButton.textContent = "toggle read";
+  toggleReadButton.addEventListener('click', (e) => {
+    obj.readstatus == "read" ? obj.readstatus = "not read" : obj.readstatus = "read";
+    libraryShelf.removeChild(libraryShelf.children[obj.dataAttr - 1]);
+    libraryShelf.insertBefore(drawLibrary(obj), libraryShelf.childNodes[obj.dataAttr]);
+  });
+
+
+  const cardMarkup = `<h1 class="card-title">${title}</h1>` + 
+  `<h3 class="card-author">By: ${author}</h3>` + 
+  `<h3 class="card-pages">Nr. of pages: ${pages}</h3>` + 
+  `<h4 class="card-read">Reading status: ${readStatus}</h4>` + 
+  `<h4 class="card-category">Category: ${category}</h4>`;
+  newDiv.innerHTML = cardMarkup;
+  newDiv.appendChild(toggleReadButton);
+
+  return newDiv
 }
